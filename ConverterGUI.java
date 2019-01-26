ef7b04759476ecf16afa2042ec79ed969dfd94c8
JStanderfer <jjstanderfer@gmail.com>
2019-01-25 20:38:16 -0800

    Update ConverterGUI.java

--- ConverterGUI.java
+++ ConverterGUI.java
@@ -1,5 +1,20 @@
 package assignmentOne;
 
 public class ConverterGUI {
+	Converter cDec;
+	Converter cHex;
+	Converter cOct;
+	Converter cBin;
+	Converter cTxt;
+	Converter cFlt;
+	
+	ConverterGUI(){
+		cDec = new Converter('d');
+		cHex = new Converter('h');
+		cOct = new Converter('o');
+		cBin = new Converter('b');
+		cTxt = new Converter('t');
+		cFlt = new Converter('f');
+	}
 
 }
